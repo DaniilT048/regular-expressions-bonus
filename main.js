6f9adfa9f389507f5c9a819929ec9f7aabe56976
DaniilT048 <tis.daniil98@gmail.com>
2025-03-07 14:09:50 +0200

    eeeee win

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
 function isValidUrl(url) {
 // return /^(https?:\/\/)([^:/]+)(?::(\d+))?([^?#]*)(\?[^#]*)?(#.*)?$/gi.test(url);
-return /^(https?:\/\/)?(www\.)?([^:/]+)(?::(\d+))?([^?#]*)(\?[^#]*)?(#.*)?$/gi.test(url)
+return /^(https?:\/\/)?(www\.)?([\w-]+\.)+[\w-]{2,}(\/[^\s]*)?$/i.test(url)
 }
 
 console.log(isValidUrl('https://www.example.com')) // Повинно вивести: true
 console.log(isValidUrl('invalid-url'))             // Повинно вивести: false
 console.log(isValidUrl('httphttphttps://www.example.com')) // Повинно вивести: false
 console.log(isValidUrl('s://www.example.com/aaaaa')) // Повинно вивести: false
-console.log(isValidUrl('google.com'))
+console.log(isValidUrl('google.com')) // Повинно вивести: true
